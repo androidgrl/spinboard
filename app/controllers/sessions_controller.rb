ca6971eaff55337eda91edf0515f52f7064c5db7
androidgrl <kawaharajamie@gmail.com>
2015-11-18 14:24:48 -0700

    Add url validation
    Using this gem: https://github.com/perfectline/validates_url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,6 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path
-    else
-      flash.now[:errors] = "Invalid login"
-      redirect_to root_path
     end
   end
 
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Link, type: :model do
 
   let(:link) { Link.new(title: "Dots",
-                        url: "www.dots.com",
+                        url: "http://www.dots.com",
                         )}
 
   it 'is valid' do
@@ -12,5 +12,11 @@ RSpec.describe Link, type: :model do
 
   it 'has a read value that defaults to false' do
     refute(link.read)
+  end
+
+  it 'validates the url' do
+    link.url = "me"
+
+    expect(link).to_not be_valid
   end
 end
