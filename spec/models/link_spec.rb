b2c7b25fcf033ae1a4cd577ceac48f6edf3fdc90
androidgrl <kawaharajamie@gmail.com>
2015-11-18 14:03:59 -0700

    Add link test
    To validate default read value of false.

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:link) { Link.new(title: "Dots",
+                        url: "www.dots.com",
+                        )}
+
+  it 'is valid' do
+    expect(link).to be_valid
+  end
+
+  it 'has a read value that defaults to false' do
+    refute(link.read)
+  end
 end
