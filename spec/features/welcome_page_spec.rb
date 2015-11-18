390559d60998df122930c691f4feeab629766482
androidgrl <kawaharajamie@gmail.com>
2015-11-18 11:20:28 -0700

    Add signup and login forms
    I put these on the welcome page.

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -7,4 +7,11 @@ describe "Welcome Page", :type => :feature do
     expect(page).to have_content("Login")
     expect(page).to have_content("Signup")
   end
+
+  xit "authenticated user sees logout link" do
+    visit root_path
+
+    expect(page).to have_content("Login")
+    expect(page).to have_content("Signup")
+  end
 end
