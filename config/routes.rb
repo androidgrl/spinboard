e25119d90ab2439b268ab677d9298a023d5a5d25
androidgrl <kawaharajamie@gmail.com>
2015-11-18 11:07:46 -0700

    Add login and signup links
    Add routes and spec.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root 'links#index'
+
+  get '/signup' => 'registrations#new'
+  post '/signup' => 'registrations#create'
+
+  delete '/logout' => 'sessions#destroy'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
 end
---
+++ spec/features/welcome_page_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "Welcome Page", :type => :feature do
+  it "unauthenticated user sees login and signup form" do
+    visit root_path
+
+    expect(page).to have_content("Login")
+    expect(page).to have_content("Signup")
+  end
+end
