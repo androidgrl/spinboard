6ef78566001ce1365e5f6fff0a186d062fb2bb2d
androidgrl <kawaharajamie@gmail.com>
2015-11-18 10:38:07 -0700

    Add database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,30 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
 require 'simplecov'
-SimpleCov.start "rails"
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
+require "database_cleaner"
+
+SimpleCov.start "rails"
 
 RSpec.configure do |config|
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
+
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do |example|
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
