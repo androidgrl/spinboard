e52d5f5f7ca9c866528ff137f8c892d144c9a032
androidgrl <kawaharajamie@gmail.com>
2015-11-18 13:27:21 -0700

    Add test for logout
    Had to add an id to the text field
    so that capybara could distinguish it from the other similarliy
    named text fields.

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -16,6 +16,23 @@ describe "Welcome Page", :type => :feature do
     fill_in "Password confirmation", with: "password"
     click_on "Signup"
 
-    expect(page).to have_content("Logout")
+    expect_user_to_be_signed_in(as: "jamie")
+  end
+
+  it 'authenticated user can logout' do
+    User.create(name: "Jamie",
+                email: "jamie@gmail.com",
+                password: "password")
+    visit root_path
+
+    fill_in "login-email", with: "jamie@gmail.com"
+    fill_in "login-password", with: "password"
+    click_on "Login"
+
+    expect_user_to_be_signed_in(as: "Jamie")
+
+    click_on "Logout"
+
+    expect_user_to_be_logged_out
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { User.new(name: "Jamie",
+                        email: "jamie@gmail.com",
+                        password: "password",
+                        )}
+
+  it "is valid" do
+    expect(user).to be_valid
+  end
 end
